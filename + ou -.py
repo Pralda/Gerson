4aa208b9f6a83c5f96a681743d7db1877996b92a
Pralda <51674052+Pralda@users.noreply.github.com>
2020-10-07 17:09:56 +0200

    Update + ou -.py

--- + ou -.py
+++ + ou -.py
@@ -1,18 +1,17 @@
-import random
+import random # importation de la bibliothèque
 
-nombreJoueur = 0
+nombreJoueur = 0  # Créations des variables
 nombreMystere = random.randint(1,100)
 condition = True
-print(nombreMystere)
-while condition:
-	nombreJoueur = int(input("Entrez un nombre : "))
 
-	if nombreJoueur > nombreMystere:
+while condition: # Boucle du test
+	nombreJoueur = int(input("Entrez un nombre : ")) # On récupère la valeur de l'utilisateur
+
+	if nombreJoueur > nombreMystere: # On teste comment est le nb Joueur comparé au nb Mystere
 		print("Le nombre Mystere est plus petit.")
 	elif nombreJoueur < nombreMystere:
-		print("Le nombre Joueur est plus grand.")
+		print("Le nombre Mystere est plus grand.")
 	elif nombreJoueur == nombreMystere:
 		print("Gagné !")
 		condition = False
-	else :
-		print("Bug")	
+	
